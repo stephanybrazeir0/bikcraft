c5c701f79aad7317d714ed4661317d7c3a5ed025
Stephany Brazeiro <stephanybrazeiro@gmail.com>
2024-01-03 07:35:21 -0300

    galeria de bicicletas

--- js/script.js
+++ js/script.js
@@ -42,3 +42,21 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+//Galeria de bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagem img");
+const galeriaContainer = document.querySelector(".bicicleta-imagem");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventoGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventoGaleria);
